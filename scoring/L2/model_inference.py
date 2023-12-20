606b535bfbee13b8f39516917d5f6f18500954d0
manavrmoorthy <manav@madstreetden.com>
2023-12-20 19:49:00 +0000

    l2

--- scoring/L2/model_inference.py
+++ scoring/L2/model_inference.py
@@ -115,10 +115,10 @@ def Ordinal_FeatEngg(lead_history_df_merged_subsetcols, scaler):
     for f in ordinal_feat_standardize + ordinal_feat_normalize:
         encoder = scaler[f]
         inv = encoder.inverse_transform(
-            lead_history_df_merged_subsetcols[f]
+            [lead_history_df_merged_subsetcols[f]]
         )
         print(f'INVERSE IS : {inv}')
-        lead_history_df_merged_subsetcols[f] = inv
+        lead_history_df_merged_subsetcols[f] = inv[0]
 
     return lead_history_df_merged_subsetcols
 
