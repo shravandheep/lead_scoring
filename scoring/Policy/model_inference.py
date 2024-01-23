a03a288f0668cc926c25504aec3599f90223daa7
Shravan Dheep <shravan@madstreetden.com>
2024-01-23 10:39:28 +0000

    testing

--- scoring/Policy/model_inference.py
+++ scoring/Policy/model_inference.py
@@ -223,18 +223,6 @@ def inference(node_dict, data, score_request):
             "score": score_ms,
             "likelihood": likelihood_ms,
             "lead_type": lead_type_f
-        },
-        {
-            "type": "confidence_scores",
-            "score": {
-                "FirstName_Match": filters_t[0]['FirstName_Match'],
-                "LastName_Match": filters_t[0]['LastName_Match'],
-                "City_Match": filters_t[0]['City_Match'],
-                "StateCode_Match": filters_t[0]['StateCode_Match'],
-                "Phone_Match_Score": filters_t[0]['Phone_Match_Score'],
-                "Email_Match_Score": filters_t[0]['Email_Match_Score'],
-                
-            }
         }
     ]
 
