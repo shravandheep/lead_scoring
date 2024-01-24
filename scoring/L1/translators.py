ef20d5c17f7024beda7e398c80402a871b630ab2
Shravan Dheep <shravan@madstreetden.com>
2024-01-24 02:45:52 +0000

    test

--- scoring/L1/translators.py
+++ scoring/L1/translators.py
@@ -161,7 +161,7 @@ class Translator(object):
             ),
             axis=1,
         )
-
+        print(new_data['MobilePhone'].empty)
         
         if new_data['MobilePhone'].empty: 
             new_data["Phone_Match_Score"] = 0
