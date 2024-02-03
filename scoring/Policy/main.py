2ea9a679def40f60d6d03077b51d6883a499f4d7
krisss08 <121148759+krisss08@users.noreply.github.com>
2024-02-03 07:48:50 +0530

    policy neustar

--- scoring/Policy/main.py
+++ scoring/Policy/main.py
@@ -133,7 +133,17 @@ def process(data, node_dict):
     neustar_data = args_dict["data"].get(NEUSTAR_DATA, {})
 
     combined_data.update(lead_data)
-    combined_data.update(neustar_data["Results"])
+    # combined_data.update(neustar_data["Results"])
+
+    try:
+        combined_data.update(neustar_data["Results"])
+    except KeyError: 
+        dummy_neustar = os.path.join(_FILE_PATH, "dummy_neustar.json")
+
+        with open(dummy_neustar, "r") as json_file:
+            dummy_neustar_cols = json.load(json_file)
+        neustar_data["Results"] = dummy_neustar_cols
+        combined_data.update(neustar_data["Results"])
 
     #     node_dict['neustar_match'] = "not_matched" if not bool(neustar_data) else "matched"
 
