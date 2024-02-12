f3a2b239ce48f57c4cce67cce413f189b245fcf1
krisss08 <121148759+krisss08@users.noreply.github.com>
2024-02-12 12:43:52 +0530

    check income group

--- scoring/L1/model_inference.py
+++ scoring/L1/model_inference.py
@@ -198,7 +198,7 @@ def inference(node_dict, data, score_request):
 
     data_subset_features = data[considered_features]
 
-    print(data_subset_features.to_dict(orient='records')[0]['Occupation_group'])
+    print(data_subset_features.to_dict(orient='records'))
 
     # Encoding
     label_encoder_dict = node_dict["label_encoders"]
